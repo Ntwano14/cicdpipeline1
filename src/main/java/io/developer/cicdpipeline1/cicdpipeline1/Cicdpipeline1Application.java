50b34cdf0f1e841609e022cce42dde4f07518a9a
Ntwano14 <ntwano14@gmail.com>
2024-01-27 17:08:43 +0200

    Initial commit

---
+++ src/main/java/io/developer/cicdpipeline1/cicdpipeline1/Cicdpipeline1Application.java
@@ -0,0 +1,13 @@
+package io.developer.cicdpipeline1.cicdpipeline1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicdpipeline1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cicdpipeline1Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/developer/cicdpipeline1/cicdpipeline1/Cicdpipeline1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.developer.cicdpipeline1.cicdpipeline1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicdpipeline1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
